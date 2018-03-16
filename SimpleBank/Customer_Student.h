d87fdf47a4dce34bcbaaac32f5c6ae96cc8505d2
yfedas21 <yfedas21@my.whitworth.edu>
2018-03-15 18:18:16 -0700

    Some minor changes... Am leaving for a few hours so I need to save my work.

--- SimpleBank/Customer_Student.h
+++ SimpleBank/Customer_Student.h
@@ -4,7 +4,14 @@
 #include "Customer.h"
 
 class Customer_Student : public Customer {
-	
+	Customer_Student() {
+		// Default constructor
+	}
+
+	const double SAVINGS_INTEREST = 3.14;
+	const double CHECK_INTEREST = 3.14;
+	const double CHECK_CHARGE = 2.17;
+	const double OVERDRAFT_PENALTY = 88;
 };
 
 #endif 
